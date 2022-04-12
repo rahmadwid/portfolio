1d7811879e83d9ffbbb61b4d90a62d40cf2424a7
rahmadwid <rahmadwidayat26@gmail.com>
2022-04-12 17:35:49 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const projects = [
+    {
+      title: "Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Aplikasi ini cocok digunakan untuk mengelola tugas sekolah, tugas kuliah, tugas kantor, bahkan tugas rumah.",
+      image: "./aktivitas-aplication.png",
+      link: "https://rahmadwid.github.io/todo-app/",
+    },
+    {
+      title: "Homestay App",
+      subtitle: "React and GoogleMap",
+      description:
+        "Aplikasi ini cocok digunakan untuk menemukan dan mengetahui informasi harga homestay wilayah Yogjakarta",
+      image: "./homestay-aplication.png",
+      link: "https://rahmadwid.github.io/homestay-app/",
+    },
+    {
+      title: "CRUD Data",
+      subtitle: "React and Express",
+      description:
+        "Fitur yang cocok digunakan untuk Mengelola data berbentuk tabel yang di ambil dari API. Silahkan klik gambar untuk masuk ke link",
+      image: "./crud-react.png",
+      link: "https://rahmadwid.github.io/table-data/",
+    },
+    {
+        title: "Login JWT",
+        subtitle: "React and Laravel",
+        description:
+          "Fitur yang cocok di gunakan untuk Authentication Login menggunakan Json web Token. Silahkan klik gambar untuk masuk ke link",
+        image: "./authentication-react.png",
+        link: "https://rahmadwid.github.io/login-jwt/",
+    }
+  ];
+
+  export const skills = ["HTML & CSS", "Javascript", "Reactjs", "Bootstrap", "Restful API", "GIT"]
+
+
